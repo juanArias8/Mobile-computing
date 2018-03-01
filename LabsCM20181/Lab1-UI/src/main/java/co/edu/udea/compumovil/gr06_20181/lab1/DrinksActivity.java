8e0a97c253fa2b19b6f6d0c87a575c5797e044fa
dareiza <dareiza@gmail.com>
2018-03-01 17:32:39 -0500

    seleccionar imagen desde la galeria

--- LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20181/lab1/DrinksActivity.java
+++ LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20181/lab1/DrinksActivity.java
@@ -1,13 +1,42 @@
 package co.edu.udea.compumovil.gr06_20181.lab1;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DrinksActivity extends AppCompatActivity {
+
+    ImageView imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks);
+
+        imagen = (ImageView)findViewById(R.id.iv_drink_add_image);
+
+    }
+
+    public void onClick(View view) {
+        cargarImagen();
+    }
+
+    private void cargarImagen(){
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione la aplicacion"),10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK){
+            Uri path=data.getData();
+            imagen.setImageURI(path);
+        }
     }
 }
--- LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20181/lab1/SplashScreen.java
+++ LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20181/lab1/SplashScreen.java
@@ -20,5 +20,8 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         }, 3000);
+
     }
+
+
 }
