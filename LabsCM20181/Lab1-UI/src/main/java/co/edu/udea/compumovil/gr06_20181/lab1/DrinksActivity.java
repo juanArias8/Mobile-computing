ef0193cc6d09bb5dfe37b993f6c1957da773bc1c
juanArias8 <juasda96@gmail.com>
2018-03-01 23:44:09 -0500

    landscape designed

--- LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20181/lab1/DrinksActivity.java
+++ LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20181/lab1/DrinksActivity.java
@@ -100,5 +100,9 @@ public class DrinksActivity extends AppCompatActivity {
 
     public void exitDrinks(View view){
         finish();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
--- LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20181/lab1/PlatesActivity.java
+++ LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20181/lab1/PlatesActivity.java
@@ -179,5 +179,9 @@ public class PlatesActivity extends AppCompatActivity {
 
     public void exitPlates(View view){
         finish();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
