3d37228cf251bba6b6ddeb051637d5d26ad957a9
juanArias8 <juasda96@gmail.com>
2018-03-01 22:46:34 -0500

    Strings file es finished

--- LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20181/lab1/DrinksActivity.java
+++ LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20181/lab1/DrinksActivity.java
@@ -6,25 +6,31 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class DrinksActivity extends AppCompatActivity {
 
-    ImageView ivDrinkImage;
+    private EditText etDrinkName;
+    private EditText etDrinkPrice;
+    private EditText etDrinkIngredients;
+    private ImageView ivDrinkImage;
+
+    private Uri path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks);
 
+        etDrinkName = (EditText) findViewById(R.id.et_drink_name);
+        etDrinkPrice = (EditText) findViewById(R.id.et_drink_price);
+        etDrinkIngredients = (EditText) findViewById(R.id.et_drink_ingredients);
         ivDrinkImage = (ImageView) findViewById(R.id.iv_drink_add_image);
+
     }
 
     public void ibLoadImage(View view) {
-        loadImage();
-    }
-
-    private void loadImage(){
         Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         intent.setType("image/");
         startActivityForResult(intent.createChooser(intent,"Select an application"),10);
@@ -34,7 +40,7 @@ public class DrinksActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK){
-            Uri path = data.getData();
+            path = data.getData();
             ivDrinkImage.setImageURI(path);
         }
     }
